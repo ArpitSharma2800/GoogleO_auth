6e92437ed91bdba380138e33fd84346630fd8aef
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-01-24 18:39:20 +0530

    google auth handler

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { login, google, logout } = require('./routes/auth');
+const { login, google, logout, redirect } = require('./routes/auth');
 const app = express();
 const router = express.Router();
-
+const passportSetup = require('./helpers/passport.js')
 
 //home route 
 router.get('/',(req, res) => {
@@ -13,6 +13,7 @@ router.get('/',(req, res) => {
 router.get('/auth/login',login);
 router.get('/auth/google',google);
 router.get('/auth/logout',logout);
+router.get('/auth/google/redirect',redirect);
 
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,17 @@
+const passport = require("passport");
+
 exports.login = async function(req,res,next){
     console.log('login'); 
 }
-exports.google = async function(req,res,next){
-    console.log('loging in');
-}
+
+exports.google = passport.authenticate('google',{
+    scope:['profile']
+})
+
 exports.logout = async function(req,res,next){
     console.log('logout');
+}
+
+exports.redirect = async function(req,res,next){
+    res.send({"message": "your eaches to callback"})
 }
